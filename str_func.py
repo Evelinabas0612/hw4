d023cedf3a78afd837160c215496e51652025a38
Evelina Baskova <evelinabas0612@gmail.com>
2023-04-02 19:36:04 +0300

    added func_title_letters

--- str_func.py
+++ str_func.py
@@ -1,5 +1,10 @@
 def func_upper_letters(text):
     return text.upper()
 
+def func_title_letters(text):
+    #Функция, которая делает заглавными первые буквы в словах из строки
+    return text.title()
+
 
 print(func_upper_letters("Маленькой ёлочке холодно зимой"))
+print(func_title_letters("Маленькой ёлочке холодно зимой"))
